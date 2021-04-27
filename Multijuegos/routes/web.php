fdcce15e0923a0390b8042a96656bb76d3c095fe
albert <albert.bernadas.fabra@gmail.com>
2021-04-27 17:41:33 +0200

    categoria blade

--- Multijuegos/routes/web.php
+++ Multijuegos/routes/web.php
@@ -29,4 +29,5 @@ Route::get('/game', [MultijuegosController::class, 'game'])->middleware(['auth']
 Route::post('/game', [MultijuegosController::class, 'game'])->middleware(['auth']);
 
 Route::get('/categoria', [MultijuegosController::class, 'categoria_show'])->middleware(['auth']);
+
 require __DIR__.'/auth.php';
